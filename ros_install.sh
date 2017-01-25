e6a651397ed29f374d1fb0b2a5d4bf59033bc973
Shankar Kulumani SEAS-Linux <skulumani@gwmail.gwu.edu>
2017-01-25 09:10:16 -0500

    Update install script

--- ros_install.sh
+++ ros_install.sh
@@ -2,9 +2,9 @@
 
 echo "First we'll install ROS"
 
-# sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-# sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
 sudo apt-get update
 
@@ -21,3 +21,5 @@ echo "Now setup ROS environment variables"
 
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
+
+sudo apt-get install python-rosinstall
